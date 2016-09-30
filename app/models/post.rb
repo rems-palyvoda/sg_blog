d95971217432f2f47e593c9bee49c89981bbf75d
rems-palyvoda <rems.palyvoda@gmail.com>
2016-09-30 07:33:17 +0300

    add validation fields create new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
 
   validates_presence_of :title, :body
+  validates :title, presence: true,
+    length: {minimum: 5, maximum: 30 }
+  validates :body, presence: true,
+    length: {minimum: 200}
 
 end
