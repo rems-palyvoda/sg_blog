67275247a16d9a735b7381f0e6d324b9db3de738
rems-palyvoda <rems.palyvoda@gmail.com>
2016-10-04 21:52:07 +0300

    add validation create and edit post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
 
-  # validates :title, presence: true,
-  #   length: {in: 5..30}
-  # validates :body, presence: true,
-  #   length: {minimum: 200}
+  validates :title, presence: true,
+    length: {in: 5..30}
+  validates :body, presence: true,
+    length: {minimum: 200}
 
 end
