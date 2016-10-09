1aad9fa28a8598a21824aa20404f8821f4f644e6
rems-palyvoda <rems.palyvoda@gmail.com>
2016-10-09 23:26:50 +0300

    different buttons if create or update post

--- test/integration/root_test.rb
+++ test/integration/root_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class RootTest < ActiveSupport::TestCase
   
   def setup
-    create_ten_posts
+    create_five_posts
     visit "/"
   end
 
@@ -11,8 +11,8 @@ class RootTest < ActiveSupport::TestCase
     assert page.has_content?("SG Blog")
   end
 
-  def test_visit_home_page_and_see_ten_post_titles
-    assert_equal 5, page.all("li.post-title").count
+  def test_visit_home_page_and_see_five_post_titles
+    assert_equal 5, page.all(".list-group-item").count
   end
 
   def test_visit_home_page_and_see_button_new_post
@@ -20,7 +20,7 @@ class RootTest < ActiveSupport::TestCase
   end
 
   def test_visit_home_page_and_click_button_new_post
-    click_link("New Post")
+    click_button("New Post")
     assert page.has_button?("Create")
   end
 
@@ -32,7 +32,7 @@ class RootTest < ActiveSupport::TestCase
     assert page.has_no_content?(last_post_title)
   end
 
-  def create_ten_posts
+  def create_five_posts
     5.times { |n| Post.create(title: "Post_#{n}", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu blandit metus. Proin luctus, nisi id elementum volutpat, erat mauris semper neque, a commodo quam ante eget risus. Sed sed cras amet.") }
   end
 end
