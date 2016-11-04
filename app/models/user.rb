bc0be956c43d542e0ea59b33e1d2e9dfc24b2edd
rems-palyvoda <rems.palyvoda@gmail.com>
2016-11-05 00:18:09 +0200

    now user must confirm his password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   validates :name,  presence: true
   validates :email, presence: true
   has_secure_password
-  validates :password, presence: true, confirmation: true 
+  validates :password, presence: true
 end
