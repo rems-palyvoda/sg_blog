4f4dac3a25e1d7400da9557bb8b42e558321e38e
rems-palyvoda <rems.palyvoda@gmail.com>
2016-11-04 19:31:51 +0200

    add tests for user show page

---
+++ test/integration/user_show_page_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UserCreatePageTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.create(name: "Test User", email: "test@user.com")
+    visit "/users/#{@user.id}"
+  end
+
+  def test_visit_user_show_page_and_see_user_information
+    assert page.has_content?(@user.name)
+    assert page.has_content?(@user.email)
+  end
+
+  def test_visit_user_show_page_and_see_user_control_links
+    assert page.has_link?("Edit User")
+    assert page.has_link?("Delete User")
+  end
+
+end
